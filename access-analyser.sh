ab1c77ab17024e931498c4e56a6169fa0d4dec41
matiasdante <matidantesarco@gmail.com>
2024-11-02 06:05:55 +0000

    Add a variable 4 the log file path

--- access-analyser.sh
+++ access-analyser.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 COUNT=10
+PATHL=/home/matutev/gitclones/devops/nginx-log-analyser/nginx-access.log
 
 echo -e "\n=== TOP ${COUNT} IP ADDR - HIGH REQUEST ===\n"
-awk '{print $1}' nginx-access.log | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNT}
+awk '{print $1}' $PATHL | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNT}
 
 echo -e "\n=== TOP ${COUNT} PATH MOST REQUESTED ===\n"
-awk '{print $7}' nginx-access.log | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNT}
+awk '{print $7}' $PATHL | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNT}
 
 echo -e "\n=== TOP ${COUNT} STATUS CODE ===\n"
-grep -oE ' [1-5][0-9]{2} ' nginx-access.log | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNT}
+grep -oE ' [1-5][0-9]{2} ' $PATHL | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNT}
 
 echo -e "\n=== TOP ${COUNTS} USER AGENTS ===\n"
-awk -F'"' '{print $6}' nginx-access.log | sort | uniq -c | sort -nr | awk '{for(i=2;i<=NF;i++) printf "%s ", $i; print "-",$1,"requests"}' | head -n $COUNT
+awk -F'"' '{print $6}' $PATHL | sort | uniq -c | sort -nr | awk '{for(i=2;i<=NF;i++) printf "%s ", $i; print "-",$1,"requests"}' | head -n $COUNT
